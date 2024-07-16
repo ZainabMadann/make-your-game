1e077b0ea398011ee4a5a5afd2f39c0bdbf189cf
yalajmi <yalajmi97@gmail.com>
2024-07-16 20:57:04 +0300

    fixed background rendering at victory/gameover screen

--- model/gameover.js
+++ model/gameover.js
@@ -1,10 +1,13 @@
 import { freezeTimer } from "../controller/scoreboard.js"
 import { freezeLasers } from "./enemyLasers.js"
-import { enemyInterval } from "../../script.js"
+import { enemyInterval, newGame } from "../../script.js"
 
 export function gameOver() {
     document.getElementById('game-over').style.visibility = 'visible'
     document.getElementById('restart-button').style.display = 'block'
+	document.getElementById("spaceship").style.display = "none"
+    document.getElementById("scoreboard").style.display = "none"
+	document.getElementsByClassName('enemy-container')[0].style.display = 'none'
 	freezeLasers()
 	freezeTimer()
 	clearInterval(enemyInterval)
@@ -13,21 +16,26 @@ export function gameOver() {
 		document.getElementById('game-over').style.display = 'none'
 		document.getElementById('restart-button').style.display = 'none'
     
-		window.location.reload()
+		window.location.reload(true)
 	}
 }
 
 export function victory() {
 	document.getElementById('victory').style.visibility = 'visible'
 	document.getElementById('restart-button').style.display = 'block'
+	document.getElementById("spaceship").style.display = "none"
+    document.getElementById("scoreboard").style.display = "none"
+	document.getElementsByClassName('enemy-container')[0].style.display = 'none'
 	freezeLasers()
 	freezeTimer()
 	clearInterval(enemyInterval)
 	
 	window.restartGame = () => {
+		document.getElementById('restart-button').addEventListener('click', newGame);
 		document.getElementById('victory').style.display = 'none'
 		document.getElementById('restart-button').style.display = 'none'
 	
-		window.location.reload()
+		window.location.reload(true)
+
 	}
 }
