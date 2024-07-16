c712f07b53aa087878227036c427b3a442e650d1
yalajmi <yalajmi97@gmail.com>
2024-07-16 20:56:31 +0300

    fixed objects still rendering in the background and added a 'p' or 'esc' to pause/resume

--- script.js
+++ script.js
@@ -20,6 +20,8 @@ function update() {
 export function newGame() {
     window.startGame = () => {
         document.getElementById("main-menu").style.display = "none"
+        document.getElementById("spaceship").style.visibility = "visible"
+        document.getElementById("scoreboard").style.visibility = "visible"
         createEnemies(2, 6)
         enemyInterval = setInterval(createEnemyLaser, 1000)
         requestAnimationFrame(update)
@@ -29,6 +31,14 @@ export function newGame() {
 
 newGame()
 
+document.addEventListener('keydown', (e) => {
+    if (e.key === 'Escape' || e.key === 'p') {
+        pauseGame()
+    } else if (e.key === 'r') {
+        restartGame()
+    }
+})
+
 window.pauseGame = () => {
     window.pause = !window.pause
     if (window.pause) {
@@ -36,6 +46,10 @@ window.pauseGame = () => {
         freezeLasers()
         freezeTimer()
         document.getElementById("pause-button").innerText = "Continue"
+        document.getElementById("restart").style.visibility = "visible"
+        window.restartGame = () => {
+            window.location.reload(true)
+        }
     } else {
         enemyInterval = setInterval(createEnemyLaser, 1000)
         resumeLasers()
@@ -43,3 +57,4 @@ window.pauseGame = () => {
         document.getElementById("pause-button").innerText = "Pause"
     }
 }
+
